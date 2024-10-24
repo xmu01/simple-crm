4d5420b48f395a59e1af76d49d9b59a01c165c30
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-10-24 12:26:18 +0200

    8th save

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { MatTooltipModule, TooltipComponent} from '@angular/material/tooltip';
@@ -11,17 +11,39 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { User } from '../../models/user.class';
 import {MatCardModule} from '@angular/material/card';
 
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, 
-    MatInputModule, MatFormFieldModule, MatDatepickerModule, MatCardModule,
+    MatInputModule, MatFormFieldModule, MatDatepickerModule, MatCardModule, FormsModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
  
+  firestore: Firestore = inject(Firestore);
+  
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
+
+  ngOnInit(): void {
+    collection(this.firestore, 'users').valueChanges.subscribe((changes: any) => {
+      console.log('Received changes from database', changes);
+    });
+
+    
+  }
+
+
   user = new User();
 
   readonly dialog = inject(MatDialog);
